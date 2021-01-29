b29d95d9b7c935a1dc0e53b80f7be41563a91db5
ravetisy <31383327+ravetisy@users.noreply.github.com>
2021-01-29 13:52:37 +0400

    Update constructSquare.py
    Updated the solution description in a more optimal way and added the actual solution.

--- constructSquare.py
+++ constructSquare.py
@@ -40,3 +40,23 @@ def isSqare(number):
         return True
     else:
         return False
+############################## new optimized version ##################
+# its more optimized is we start to look for numbers already in square root range of possible soolutions, and if found, rise them to square.
+# and one more thing thats makes more easier the solution. In my first solution I considered the all permutations  of current string, that can make the number.
+# but this is not needed as we can assign any number to any letter, so the permutation doesnt matter any more. The only thing that matters, are the exact maaping 
+# and probable corresposndence of string to leteer. This means that we have to check weahter there are the same amount of chars<=>numbers in both string and number.
+# for examble 55687 = aabce, There is no need to check aabce aacbe aaebc versions, The have to only check that there is dsitinct 3 chars in letter and 3 distinct
+# numbers in number.
+# So goes the solution.
+
+def constructSquare(s):
+    for x in range(int(math.sqrt(10**len(s))),0,-1):
+        if count(s)==count(str(x**2)):
+            return (x**2)
+    return -1
+    
+def count(x):
+    l=[]
+    for i in set(x):
+        l.append(x.count(i))
+    return(sorted(l))
